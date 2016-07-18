64ff7884d00ec927e318656f0eb4329cb1e60f95
TuGG <tugg.solo@gmail.com>
2016-07-19 00:54:26 +0700

    test

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -23,6 +23,6 @@ $(document).ready(function () {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) { return; }
     js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
+    js.src = "//connect.facebook.net/en_US/all.js";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
