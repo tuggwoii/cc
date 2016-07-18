afbd3a73626b25e33d25a46cd2cce3dafe3aee38
TuGG <tugg.solo@gmail.com>
2016-07-19 00:43:27 +0700

    fuck azure with fb

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -23,6 +23,6 @@ $(document).ready(function () {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) { return; }
     js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js#version=v2.6";
+    js.src = "https://connect.facebook.net/en_US/sdk.js#version=v2.6";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
