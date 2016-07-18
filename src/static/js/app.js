36887bcdc637c984e96042abb2e658cdac0bae95
TuGG <tugg.solo@gmail.com>
2016-07-18 23:59:02 +0700

    fb version

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -23,6 +23,6 @@ $(document).ready(function () {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) { return; }
     js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
