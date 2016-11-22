e93a3d8adf0ce90a99d680a15ba90b6d6c07b12a
TuGG <tugg.solo@gmail.com>
2016-11-23 00:22:36 +0700

    totally fix nav active

--- src/static/js/modules/app/controllers/nav-controller.js
+++ src/static/js/modules/app/controllers/nav-controller.js
@@ -10,10 +10,10 @@ module.controller('NavController', ['$scope', 'PageService',
                     if (m.isMenu && !m.isSys) {
                         $scope.menu.push(m);
                     }
-                    console.log(location.href);
                     var url = decodeURIComponent(location.href).replace('http://127.0.0.1:8000', '');
                     url = url.replace('http://www.carcarenote.com','');
-                    url = url.replace('http://carcarenote.com','');
+                    url = url.replace('http://carcarenote.com', '');
+                    url = url.split('?')[0];
                     if (url == m.url) {
                         m.isActive = true;
                     }
